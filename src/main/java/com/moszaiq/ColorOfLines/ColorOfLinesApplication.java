3d2e121b9c3f16b2e5c6d63f837323e6ba82bd12
The Moszaiq <themoszaiq@gmail.com>
2024-09-24 13:39:02 +0530

    Color oflines website new project

---
+++ src/main/java/com/moszaiq/ColorOfLines/ColorOfLinesApplication.java
@@ -0,0 +1,13 @@
+package com.moszaiq.ColorOfLines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorOfLinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorOfLinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moszaiq/ColorOfLines/ColorOfLinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moszaiq.ColorOfLines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorOfLinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
